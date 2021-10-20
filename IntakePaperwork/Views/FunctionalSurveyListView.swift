ebfb9d3f7aeabf08f65833c3ae62a67e90438379
Dax Gerber <dax.gerber@gmail.com>
2021-10-19 19:21:22 -0600

    Add styling to the FunctionalSurveyListView and LanguageListView for Background color, Rectangles, and navigation bar title. Change Navigation bar title to inline.

--- IntakePaperwork/Views/FunctionalSurveyListView.swift
+++ IntakePaperwork/Views/FunctionalSurveyListView.swift
@@ -14,22 +14,41 @@ struct FunctionalSurveyListView: View {
     var language: String
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(model.surveys) { survey in
-                    if survey.language == language {
-                        NavigationLink {
-                            SurveyView(survey: survey)
-                                .onAppear {
-                                    model.appendArray(survey.questions.count)
+        ZStack {
+            Rectangle()
+                .aspectRatio(contentMode: .fill)
+                .foregroundColor(Color.init(.sRGB, red: 0.3, green: 0.7, blue: 1, opacity: 0.6))
+                .ignoresSafeArea()
+            ScrollView {
+                GeometryReader { geo in
+                    LazyVStack(spacing: 20) {
+                        ForEach(model.surveys) { survey in
+                            if survey.language == language {
+                                NavigationLink {
+                                    SurveyView(survey: survey)
+                                        .onAppear {
+                                            model.appendArray(survey.questions.count)
+                                        }
+                                } label: {
+                                        ZStack {
+                                            Rectangle()
+                                                .foregroundColor(.white)
+                                                .cornerRadius(5)
+                                                .shadow(radius: 5)
+                                                .frame(height: 75)
+                                                .padding(.horizontal, geo.size.width*2/9)
+                                            Text(survey.name)
+                                                .font(.title)
+                                                .bold()
+                                                .foregroundColor(.black)
+                                        }
                                 }
-                        } label: {
-                            Text(survey.name)
-                                .foregroundColor(.black)
+                            }
                         }
-                    }
+                    }.padding()
+                        .padding(.top, 30)
                 }
-            }
+            }.navigationBarTitle("Select Outcome Measure", displayMode: .inline)
         }
     }
 }
--- IntakePaperwork/Views/LanguageListView.swift
+++ IntakePaperwork/Views/LanguageListView.swift
@@ -11,27 +11,41 @@ struct LanguageListView: View {
     
     @EnvironmentObject var model: SurveyModel
     
+    init() {
+            UINavigationBar.appearance().titleTextAttributes = [.font : UIFont(name: "Georgia-Bold", size: 30)!]
+        }
+    
     var body: some View {
         NavigationView {
-            VStack {
-                NavigationLink {
-                    FunctionalSurveyListView(language: "English")
-                } label: {
-                    RectangleView(language: "English")
-                        .padding()
-                        .foregroundColor(.black)
-                }
-                NavigationLink {
-                    FunctionalSurveyListView(language: "Spanish")
-                } label: {
-                    RectangleView(language: "Spanish")
-                        .padding()
-                        .foregroundColor(.black)
-                }
-                Spacer()
+            ZStack {
+                Rectangle()
+                    .aspectRatio(contentMode: .fill)
+                    .foregroundColor(Color.init(.sRGB, red: 0.3, green: 0.7, blue: 1, opacity: 0.6))
+                    .ignoresSafeArea()
+                GeometryReader { geo in
+                    VStack {
+                        NavigationLink {
+                            FunctionalSurveyListView(language: "English")
+                        } label: {
+                            RectangleView(language: "English")
+                                .padding()
+                                .padding(.horizontal, geo.size.width*3/18)
+                                .foregroundColor(.black)
+                        }
+                        NavigationLink {
+                            FunctionalSurveyListView(language: "Spanish")
+                        } label: {
+                            RectangleView(language: "Spanish")
+                                .padding()
+                                .foregroundColor(.black)
+                                .padding(.horizontal, geo.size.width*3/18)
+                        }
+                        Spacer()
 
-            }.navigationBarTitle("Select language")
-                .padding(.top)
+                    }.navigationBarTitle("Select language", displayMode: .inline)
+                        .padding(.top, 30)
+                }
+            }
         }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
