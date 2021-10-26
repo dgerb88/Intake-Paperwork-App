7272216e33a2d2877eae84a9e2f7f96afb1d6114
Dax Gerber <dax.gerber@gmail.com>
2021-10-26 00:16:24 -0600

    Add singular views for each survey and modified survey view accordingly. Finish QuickDash completely.

---
+++ IntakePaperwork/Views/LefsView.swift
@@ -0,0 +1,31 @@
+//
+//  LefsView.swift
+//  IntakePaperwork
+//
+//  Created by Dax Gerber on 10/25/21.
+//
+
+import SwiftUI
+
+struct LefsView: View {
+    
+    @EnvironmentObject var model: SurveyModel
+    var survey: Survey
+    
+    var body: some View {
+        ForEach(0..<survey.questions.count) { index in
+            VStack(alignment: .leading) {
+                Text("\(survey.questions[index].title)")
+                    .font(.headline)
+                    .padding(.bottom, 5)
+                Picker("", selection: $model.selectedValue[index]) {
+                    ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
+                        Text(String(ratingIndex)).tag(ratingIndex)
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+                
+            }.padding(.bottom, 40).padding(.top, 10)
+        }
+    }
+}
+
--- IntakePaperwork/Views/SurveyView.swift
+++ IntakePaperwork/Views/SurveyView.swift
@@ -13,6 +13,7 @@ struct SurveyView: View {
     var survey: Survey
     @State var score = 0
     @State var finished = false
+    var truth = true
     
     var body: some View {
         ZStack {
@@ -59,216 +60,28 @@ struct SurveyView: View {
                                 VStack(alignment: .leading) {
                                     
                                     if survey.name == "LEFS" {
-                                        ForEach(0..<survey.questions.count) { index in
-                                            VStack(alignment: .leading) {
-                                                Text("\(survey.questions[index].title)")
-                                                    .font(.headline)
-                                                    .padding(.bottom, 5)
-                                                Picker("", selection: $model.selectedValue[index]) {
-                                                    ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                                                        Text(String(ratingIndex)).tag(ratingIndex)
-                                                    }
-                                                }.pickerStyle(SegmentedPickerStyle())
-                                                
-                                            }.padding(.bottom, 40).padding(.top, 10)
-                                        }
+                                        LefsView(survey: survey)
                                     }
                                     else if survey.name == "Back Index" {
-                                        ForEach(0..<survey.questions.count) { index in
-                                            VStack(alignment: .leading) {
-                                                Text("\(survey.questions[index].title)")
-                                                    .font(.title)
-                                                Picker("", selection: $model.selectedValue[index]) {
-                                                    ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                                                        Text(String(ratingIndex)).tag(ratingIndex)
-                                                    }
-                                                }.pickerStyle(SegmentedPickerStyle())
-                                                ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                                                    Text(survey.questions[index].rating[ratingIndex]).tag(ratingIndex)
-                                                }.padding(.leading, 10)
-                                                
-                                                
-                                            }.padding(.vertical, 20)
-                                        }
+                                        BackIndexView(survey: survey)
                                     }
-                                    else if survey.name == "QuickDash" {
-                                        VStack(alignment: .leading) {
-                                            HStack {
-                                                Spacer()
-                                                HStack(spacing: 15) {
-                                                    VStack {
-                                                        Text("No")
-                                                        Text("difficulty")
-                                                    }
-                                                    VStack {
-                                                        Text("Mild")
-                                                        Text("difficulty")
-                                                    }
-                                                    VStack {
-                                                        Text("Moderate")
-                                                        Text("difficulty")
-                                                    }
-                                                    VStack {
-                                                        Text("Severe")
-                                                        Text("difficulty")
-                                                    }
-                                                    VStack {
-                                                        Text("Unable")
-                                                        Text("to do")
-                                                    }
-                                                }.frame(width: 410)
-                                            }
-                                            ForEach(0..<6) { index in
-                                                VStack(alignment: .leading) {
-                                                    HStack {
-                                                        Text("\(index+1). \(survey.questions[index].title)")
-                                                        Spacer()
-                                                        
-                                                        VStack {
-                                                            Picker("", selection: $model.selectedValue[index]) {
-                                                                ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                                                                    Text(String(ratingIndex+1)).tag(ratingIndex)
-                                                                }
-                                                            }.pickerStyle(SegmentedPickerStyle())
-                                                                .frame(width: 410)
-                                                            Spacer()
-                                                        }
-                                                        
-                                                    }
-                                                }.padding(.bottom, 10)
-                                            }
-                                            HStack {
-                                                Spacer()
-                                                HStack {
-                                                    VStack {
-                                                        Text("Not at all")
-                                                    }
-                                                    VStack {
-                                                        Text("Slightly")
-                                                    }
-                                                    VStack {
-                                                        Text("Moderately")
-                                                    }
-                                                    VStack {
-                                                        Text("Quite a bit")
-                                                    }
-                                                    VStack {
-                                                        Text("Extremely")
-                                                    }
-                                                }.frame(width: 410)
-                                                    .padding(.bottom, 10)
-                                            }
-                                            HStack {
-                                                Text("\(7). \(survey.questions[6].title)")
-                                                    .padding(.leading, 5)
-                                                Spacer()
-                                                
-                                                VStack {
-                                                    Picker("", selection: $model.selectedValue[6]) {
-                                                        ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
-                                                            Text(String(ratingIndex+1)).tag(ratingIndex)
-                                                        }
-                                                    }.pickerStyle(SegmentedPickerStyle())
-                                                        .frame(width: 400)
-                                                    Spacer()
-                                                }
-                                            }
-                                            HStack {
-                                                Spacer()
-                                                HStack {
-                                                    VStack {
-                                                        Text("Not limited")
-                                                        Text("at all")
-                                                    }
-                                                    VStack {
-                                                        Text("Slightly")
-                                                        Text("limited")
-                                                    }
-                                                    VStack {
-                                                        Text("Moderately")
-                                                        Text("limited")
-                                                    }
-                                                    VStack {
-                                                        Text("Very")
-                                                        Text("limited")
-                                                    }
-                                                    VStack {
-                                                        Text("Unable")
-                                                        Text("to do")
-                                                    }
-                                                }.frame(width: 410)
-                                                    .padding(.vertical, 10)
-                                            }
-                                            HStack {
-                                                Text("\(8). \(survey.questions[7].title)")
-                                                    .padding(.leading, 5)
-                                                Spacer()
-                                                
-                                                VStack {
-                                                    Picker("", selection: $model.selectedValue[7]) {
-                                                        ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
-                                                            Text(String(ratingIndex+1)).tag(ratingIndex)
-                                                        }
-                                                    }.pickerStyle(SegmentedPickerStyle())
-                                                        .frame(width: 400)
-                                                    Spacer()
-                                                }
-                                            }
-                                            VStack {
-                                                Text("Please rate the severity of the following symptoms in the last week")
-                                                    .bold()
-                                                    .padding(.vertical, 20)
-                                            }
-                                            HStack {
-                                                Spacer()
-                                                HStack {
-                                                    Text("None").padding(.leading, 20)
-                                                    Text("Mild").padding(.trailing, 10).padding(.leading, 30)
-                                                    Text("Moderate").padding(.trailing, 20).padding(.leading, 10)
-                                                    Text("Severe").padding(.trailing, 10)
-                                                    Text("Extreme")
-                                                }.frame(width: 410)
-                                                    .padding(.bottom, 10)
-                                            }
-                                            HStack {
-                                                Text("\(9). \(survey.questions[8].title)")
-                                                    .padding(.leading, 5)
-                                                Spacer()
-                                                
-                                                VStack {
-                                                    Picker("", selection: $model.selectedValue[8]) {
-                                                        ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
-                                                            Text(String(ratingIndex+1)).tag(ratingIndex)
-                                                        }
-                                                    }.pickerStyle(SegmentedPickerStyle())
-                                                        .frame(width: 400)
-                                                    Spacer()
-                                                }
-                                            }
-                                            HStack {
-                                                Text("\(10). \(survey.questions[9].title)")
-                                                    .padding(.leading, 5)
-                                                Spacer()
-                                                
-                                                VStack {
-                                                    Picker("", selection: $model.selectedValue[9]) {
-                                                        ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
-                                                            Text(String(ratingIndex+1)).tag(ratingIndex)
-                                                        }
-                                                    }.pickerStyle(SegmentedPickerStyle())
-                                                        .frame(width: 400)
-                                                    Spacer()
-                                                }
-                                            }
-                                        }
+                                    else if survey.name == "QuickDash" && survey.language == "English" {
+                                        QuickDashEngView(survey: survey)
                                     }
+                                    
                                     else {
                                         Text("Survey not found")
                                     }
                                     Button {
                                         if finished != true {
                                             for index in 0..<model.selectedValue.count {
-                                                score += model.selectedValue[index]
+                                                if survey.name == "QuickDash" {
+                                                    score += model.selectedValue[index] + 1
+                                                }
+                                                else {
+                                                    score += model.selectedValue[index]
+
+                                                }
                                             }
                                         }
                                         finished = true
@@ -287,6 +100,11 @@ struct SurveyView: View {
                                             }
                                             else if finished && survey.name == "Back Index" {
                                                 Text("Score: \(Int(Double(score*100/50)))%")
+                                                    .foregroundColor(.white)
+                                                    .bold()
+                                            }
+                                            else if finished && survey.name == "QuickDash" {
+                                                Text("Score: \(Int(Double(score/11)-1)*25)/100")
                                                     .foregroundColor(.white)
                                                     .bold()
                                             }
