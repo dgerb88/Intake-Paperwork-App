6779f2883575c7e8d8fdd1d1713d209b09939715
Dax Gerber <dax.gerber@gmail.com>
2021-10-28 15:30:00 -0600

    Same as last push

--- IntakePaperwork/Views/BackIndexView.swift
+++ IntakePaperwork/Views/BackIndexView.swift
@@ -13,25 +13,68 @@ struct BackIndexView: View {
     var survey: Survey
     
     var body: some View {
-        ForEach(0..<survey.questions.count) { index in
-            VStack(alignment: .leading) {
-                Text("\(survey.questions[index].title)")
-                    .font(.title)
-                Picker("", selection: $model.selectedValue[index]) {
-                    ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                        Text(String(ratingIndex)).tag(ratingIndex)
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .shadow(radius: 5)
+            VStack {
+                Text(survey.description)
+                    .font(.headline)
+                    .padding(.bottom, 10)
+                Divider()
+                ForEach(0..<survey.questions.count) { index in
+                    VStack(alignment: .leading) {
+                        Text("\(survey.questions[index].title)")
+                            .font(.title)
+                        Picker("", selection: $model.selectedValue[index]) {
+                            ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
+                                Text(String(ratingIndex)).tag(ratingIndex)
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                            .onChange(of: model.selectedValue) { newValue in
+                                model.finishedSurvey = false
+                                model.score = 0
+                            }
+                        ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
+                            Text(survey.questions[index].rating[ratingIndex]).tag(ratingIndex)
+                        }.padding(.leading, 10)
+                        
+                        
                     }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .onChange(of: model.selectedValue) { newValue in
-                        model.finishedSurvey = false
-                        model.score = 0
+                }
+                Button {
+                    if model.finishedSurvey != true {
+                        for index in 0..<model.selectedValue.count {
+                            model.score += model.selectedValue[index]
+                        }
                     }
-                ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                    Text(survey.questions[index].rating[ratingIndex]).tag(ratingIndex)
-                }.padding(.leading, 10)
-                
-                
-            }.padding(.vertical, 20)
+                    model.finishedSurvey = true
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.green)
+                            .frame(height: 48)
+                            .cornerRadius(10)
+                            .shadow(radius: 1)
+                        if model.finishedSurvey {
+                            Text("Score: \(Int(Double(model.score*100/50)))%")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                        
+                        else {
+                            Text("Finish")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                    }
+                    
+                }.padding(.vertical, 5)
+            }.onAppear {
+                model.score = 0
+            }.padding()
         }
     }
 }
--- IntakePaperwork/Views/LefsView.swift
+++ IntakePaperwork/Views/LefsView.swift
@@ -13,22 +13,84 @@ struct LefsView: View {
     var survey: Survey
     
     var body: some View {
-        ForEach(0..<survey.questions.count) { index in
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .shadow(radius: 5)
             VStack(alignment: .leading) {
-                Text("\(survey.questions[index].title)")
+                Text(survey.description)
                     .font(.headline)
-                    .padding(.bottom, 5)
-                Picker("", selection: $model.selectedValue[index]) {
-                    ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
-                        Text(String(ratingIndex)).tag(ratingIndex)
+                    .padding(.bottom, 10)
+                if survey.language == "English" {
+                    Text("The following scale applies to every question:")
+                        .font(.headline)
+                        .bold()
+                        .padding(.bottom, 5)
+                        
+                }
+                else {
+                    Text("El siguiente escala aplica a cada pregunta:")
+                        .font(.headline)
+                        .bold()
+                        .padding(.bottom, 5)
+                }
+                ForEach(0..<survey.questions[0].rating.count) { index in
+                    Text(survey.questions[0].rating[index])
+                        .font(.subheadline)
+                        .padding(.bottom, 1)
+                        .padding(.leading, 20)
+                }
+                Divider()
+                ForEach(0..<survey.questions.count) { index in
+                    VStack(alignment: .leading) {
+                        Text("\(survey.questions[index].title)")
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                        Picker("", selection: $model.selectedValue[index]) {
+                            ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
+                                Text(String(ratingIndex)).tag(ratingIndex)
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                            .onChange(of: model.selectedValue) { newValue in
+                                model.finishedSurvey = false
+                                model.score = 0
+                            }
+                        
+                    }.padding(.bottom, 40).padding(.top, 10)
+                }
+                Button {
+                    if model.finishedSurvey != true {
+                        for index in 0..<model.selectedValue.count {
+                            model.score += model.selectedValue[index]
+                        }
                     }
-                }.pickerStyle(SegmentedPickerStyle())
-                    .onChange(of: model.selectedValue) { newValue in
-                        model.finishedSurvey = false
-                        model.score = 0
+                    model.finishedSurvey = true
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.green)
+                            .frame(height: 48)
+                            .cornerRadius(10)
+                            .shadow(radius: 1)
+                        if model.finishedSurvey {
+                            Text("Score: \(model.score)/80")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
+                        else {
+                            Text("Finish")
+                                .foregroundColor(.white)
+                                .bold()
+                        }
                     }
-                
-            }.padding(.bottom, 40).padding(.top, 10)
+                    
+                }.padding(.top, 5)
+            }.padding()
+        }
+        .onAppear {
+            model.score = 0
         }
     }
 }
