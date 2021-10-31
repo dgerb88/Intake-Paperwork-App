49b7d96d673ea706e5e7e1f5a881300e373e0c96
Dax Gerber <dax.gerber@gmail.com>
2021-10-30 22:20:32 -0600

    add QuickDashSpanView, copy from english view and change to spanish.

---
+++ IntakePaperwork/Views/Survey Views/QuickDashSpanView.swift
@@ -0,0 +1,291 @@
+//
+//  QuickDashSpanView.swift
+//  IntakePaperwork
+//
+//  Created by Dax Gerber on 10/30/21.
+//
+
+import SwiftUI
+
+struct QuickDashSpanView: View {
+    
+    @EnvironmentObject var model: SurveyModel
+    var survey: Survey
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(5)
+                .shadow(radius: 5)
+            VStack {
+                Text(survey.description)
+                    .font(.headline)
+                    .padding(.bottom, 10)
+                Divider()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Spacer()
+                        HStack(spacing: 15) {
+                            VStack {
+                                Text("Ninguna")
+                                Text("dificultad")
+                            }
+                            VStack {
+                                Text("Dificultad")
+                                Text("leve")
+                            }
+                            VStack {
+                                Text("Dificultad")
+                                Text("moderada")
+                            }
+                            VStack {
+                                Text("Dificultad")
+                                Text("severa")
+                            }
+                            VStack {
+                                Text("No lo")
+                                Text("puedo")
+                                Text("ejecutar")
+                            }
+                        }.frame(width: 410)
+                    }
+                    ForEach(0..<6) { index in
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("\(index+1). \(survey.questions[index].title)")
+                                Spacer()
+                                
+                                VStack {
+                                    Picker("", selection: $model.selectedValue[index]) {
+                                        ForEach(0..<survey.questions[index].rating.count) { ratingIndex in
+                                            Text(String(ratingIndex+1)).tag(ratingIndex)
+                                        }
+                                    }.pickerStyle(SegmentedPickerStyle())
+                                        .frame(width: 410)
+                                        .onChange(of: model.selectedValue) { newValue in
+                                            model.finishedSurvey = false
+                                            model.score = 0
+                                        }
+                                    Spacer()
+                                }
+                                
+                            }
+                        }.padding(.bottom, 10)
+                    }
+                    Divider()
+                        .foregroundColor(.black)
+                    HStack {
+                        Spacer()
+                        HStack {
+                            VStack {
+                                Text("Para")
+                                Text("nada")
+                            }
+                            VStack {
+                                Text("Un")
+                                Text("Poco")
+                            }
+                            VStack {
+                                Text("Moderado")
+                            }
+                            VStack {
+                                Text("Bastante")
+                            }
+                            VStack {
+                                Text("Incapaz")
+                            }
+                        }.frame(width: 410)
+                            .padding(.bottom, 10)
+                    }
+                    HStack {
+                        Text("\(7). \(survey.questions[6].title)")
+                            .padding(.leading, 5)
+                        Spacer()
+                        
+                        VStack {
+                            Picker("", selection: $model.selectedValue[6]) {
+                                ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
+                                    Text(String(ratingIndex+1)).tag(ratingIndex)
+                                }
+                            }.pickerStyle(SegmentedPickerStyle())
+                                .frame(width: 400)
+                            Spacer()
+                        }
+                    }
+                    Divider()
+                        .foregroundColor(.black)
+                    HStack {
+                        Spacer()
+                        HStack {
+                            VStack {
+                                Text("Para")
+                                Text("nada")
+                            }
+                            VStack {
+                                Text("Un")
+                                Text("poco")
+                            }
+                            VStack {
+                                Text("Con")
+                                Text("moderación")
+                            }
+                            VStack {
+                                Text("Bastante")
+                                Text("limitado")
+                            }
+                            VStack {
+                                Text("Limitado/a")
+                                Text("Totalmente")
+                            }
+                        }.frame(width: 410)
+                            .padding(.vertical, 10)
+                    }
+                    HStack {
+                        Text("\(8). \(survey.questions[7].title)")
+                            .padding(.leading, 5)
+                        Spacer()
+                        
+                        VStack {
+                            Picker("", selection: $model.selectedValue[7]) {
+                                ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
+                                    Text(String(ratingIndex+1)).tag(ratingIndex)
+                                }
+                            }.pickerStyle(SegmentedPickerStyle())
+                                .frame(width: 400)
+                            Spacer()
+                        }
+                    }
+                    Divider()
+                        .foregroundColor(.black)
+                }
+                VStack(alignment: .leading) {
+                    Text("Por favor califique la gravedad de los síntomas siguientes durante la última semana")
+                        .bold()
+                        .padding(.vertical, 20)
+                    
+                    HStack {
+                        Spacer()
+                        HStack {
+                            VStack {
+                                Text("Ningún")
+                                Text("Síntoma")
+                            }.padding(.leading, 20)
+                            Text("Leve").padding(.trailing, 10).padding(.leading, 30)
+                            Text("Moderado").padding(.trailing, 20).padding(.leading, 10)
+                            Text("Severo").padding(.trailing, 10)
+                            Text("Extremo")
+                        }.frame(width: 410)
+                            .padding(.bottom, 10)
+                    }
+                    HStack {
+                        Text("\(9). \(survey.questions[8].title)")
+                            .padding(.leading, 5)
+                        Spacer()
+                        
+                        VStack {
+                            Picker("", selection: $model.selectedValue[8]) {
+                                ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
+                                    Text(String(ratingIndex+1)).tag(ratingIndex)
+                                }
+                            }.pickerStyle(SegmentedPickerStyle())
+                                .frame(width: 400)
+                            
+                        }
+                    }.padding(.bottom, 20)
+                    HStack {
+                        Text("\(10). \(survey.questions[9].title)")
+                            .padding(.leading, 5)
+                        Spacer()
+                        
+                        VStack {
+                            Picker("", selection: $model.selectedValue[9]) {
+                                ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
+                                    Text(String(ratingIndex+1)).tag(ratingIndex)
+                                }
+                            }.pickerStyle(SegmentedPickerStyle())
+                                .frame(width: 400)
+                                .padding(.bottom, 20)
+                        }
+                    }
+                    Divider()
+                    HStack {
+                        Spacer()
+                        HStack {
+                            VStack {
+                                Text("Ninguna")
+                                Text("dificultad")
+                            }
+                            VStack {
+                                Text("Dificultad")
+                                Text("leve")
+                            }
+                            VStack {
+                                Text("Dificultad")
+                                Text("moderada")
+                            }
+                            VStack {
+                                Text("Dificultad")
+                                Text("severa")
+                            }
+                            VStack {
+                                Text("Tanto, que")
+                                Text("no puedo")
+                                Text("dormir")
+                            }
+                        }.frame(width: 410)
+                            .padding(.vertical, 10)
+                    }
+                    HStack {
+                        Text("\(11). \(survey.questions[10].title)")
+                            .padding(.leading, 5)
+                        Spacer()
+                        
+                        VStack {
+                            Picker("", selection: $model.selectedValue[10]) {
+                                ForEach(0..<survey.questions[0].rating.count) { ratingIndex in
+                                    Text(String(ratingIndex+1)).tag(ratingIndex)
+                                }
+                            }.pickerStyle(SegmentedPickerStyle())
+                                .frame(width: 400)
+                                .padding(.bottom, 50)
+                        }
+                    }
+                    Button {
+                        if model.finishedSurvey != true {
+                            for index in 0..<model.selectedValue.count {
+                                model.score += model.selectedValue[index] + 1
+                            }
+                            
+                        }
+                        model.finishedSurvey = true
+                        
+                    } label: {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(.green)
+                                .frame(height: 48)
+                                .cornerRadius(10)
+                                .shadow(radius: 1)
+                            if model.finishedSurvey {
+                                Text("Puntuación: \(model.score)/55")
+                                    .foregroundColor(.white)
+                                    .bold()
+                            }
+                            else {
+                                Text("Terminar")
+                                    .foregroundColor(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                    }.padding(.vertical, 5)
+                }.onAppear {
+                    model.score = 0
+                }
+            }.padding()
+        }
+
+    }
+}
+
